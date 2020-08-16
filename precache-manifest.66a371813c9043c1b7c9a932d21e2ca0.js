ba7e76e458a22a0db61beb2c0e2e207d111e6d07
Viktor Åsbrink <viktorasbrink@gmail.com>
2020-08-16 18:40:59 +0200

    Updates

---
+++ precache-manifest.66a371813c9043c1b7c9a932d21e2ca0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e582903ebcab8ca360fd110ef61fac68",
+    "url": "/tictactoe_react_practice/index.html"
+  },
+  {
+    "revision": "8b6d609641004a8816ae",
+    "url": "/tictactoe_react_practice/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "f75f61e4f4395e630f7f",
+    "url": "/tictactoe_react_practice/static/js/2.19d730dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe_react_practice/static/js/2.19d730dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b6d609641004a8816ae",
+    "url": "/tictactoe_react_practice/static/js/main.50f2ec17.chunk.js"
+  },
+  {
+    "revision": "8f7a03a613c3201f6cf5",
+    "url": "/tictactoe_react_practice/static/js/runtime-main.e2f7c4f1.js"
+  }
+]);
